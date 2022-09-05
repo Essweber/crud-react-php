e65508ad4dda8354ae46c9e9805741ce86bac395
Ess_Weber <aessotchossim@gmail.com>
2022-09-05 15:26:13 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h5>React crud and php</h5>
+
+    <BrowserRouter>
+    <nav>
+      <ul>
+        <li><Link to='/'>List users</Link></li>
+        <li><Link to='user/create'>create user</Link></li>
+      </ul>
+    </nav>
+    </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
